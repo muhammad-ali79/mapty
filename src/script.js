b9e7eeffa41030ee1ae6b9e1792c0fb2373e7c0a
muhammad-ali79 <alihelium369@gmail.com>
2023-12-24 10:19:38 +0500

    location is accessed using the api

--- src/script.js
+++ src/script.js
@@ -4,3 +4,22 @@ zoom
 drag
 show marker
 */
+
+// get the user location using the geolocation Api
+
+let lat, lon;
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+    });
+  } else {
+    prompt("could not get your location");
+  }
+};
+
+window.onload = () => {
+  getLocation();
+};
